53907b74148063bed8a97de5b186071425e2dc74
Seven0317 <627517032@qq.com>
2018-09-07 00:27:23 +0800

    First version for Auto Excitation

---
+++ AutoExcitation/log.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Version : V1.0
+# Author  : Seven
+# Date    : 2018/9/2 14:20
+
+import os
+import yaml
+import logging
+import logging.config
+
+
+def setup_logging(default_path='logging.yaml', default_level=logging.INFO):
+    """
+    Setup logging configuration
+    """
+    path = default_path
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = yaml.load(f.read())
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
+        print(r'The input path doesn\'t exist')
---
+++ AutoExcitation/main.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Version : V1.0
+# Author  : Seven
+# Date    : 2018/9/3 14:38
+
+import os
+import logging
+from log import setup_logging
+from crawler.main_spider import spider
+from displayer.main_window import display, app
+
+# Set up logger by log.yaml
+setup_logging(default_path=os.path.join(os.getcwd(), r"log.yaml"))
+logger = logging.getLogger("fileLogger")
+# Init app
+try:
+    App = app
+    logger.info("Initialise app successfully.")
+except Exception as e:
+    logger.error(e)
+    logger.error("Initialise app failed")
+# Crawl data
+try:
+    spider()
+except Exception as e:
+    logger.error(e)
+    logger.error("Crawl data failed.")
+# Display text
+try:
+    display()
+except Exception as e:
+    logger.error(e)
+    logger.error("Display text failed.")
